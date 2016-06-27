7538fbcc8ffb317d4a2295756ceb42e4e4f0bb57
Nikola Lukić <luknik94@gmail.com>
2016-06-27 18:25:59 +0200

    initial commit

---
+++ app/src/main/java/rs/ac/uns/ftn/luxa/dotsandboxes/MainActivity.java
@@ -0,0 +1,10 @@
+package rs.ac.uns.ftn.luxa.dotsandboxes;
+
+import android.support.v7.app.AppCompatActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+
+}
